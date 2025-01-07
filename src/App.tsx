623c11a5d47562ac11d39f841dbff0ed0152f9b0
atharva <atharvapuranik251002@gmail.com>
2025-01-07 22:05:11 +0530

    removed unnecessary imports and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import LoadingScreen from './components/LoadingScreen';
 import HomeScreen from './components/HomeScreen';
 
